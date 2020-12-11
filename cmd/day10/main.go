3cc32b70884eb2dc80dc2462b88cb514d38509fc
Bowman,Roger <Roger.Bowman@53.com>
2020-12-11 15:13:02 -0500

    day 10 puzzles 1 & 2

--- cmd/day10/main.go
+++ cmd/day10/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/bowmanrogere/advent-of-code-2020/internal"
+	"github.com/bowmanrogere/advent-of-code-2020/internal/graph"
 	"log"
 	"os"
 	"sort"
@@ -16,12 +17,14 @@ func main() {
 
 	adapters := internal.ConvertToInts(lines)
 	sort.Ints(adapters)
-	// add built in adapter
-	adapters = append(adapters, adapters[len(adapters)-1] + 3)
 	puzzle1(adapters)
+	puzzle2(adapters)
 }
 
 func puzzle1(adapters []int) {
+	// add built in adapter
+	adapters = append(adapters, adapters[len(adapters)-1] + 3)
+
 	differences := make(map[int][]int)
 
 	previous := 0
@@ -36,4 +39,47 @@ func puzzle1(adapters []int) {
 	println(fmt.Sprintf("Puzzle #1: %d", len(differences[1]) * len(differences[3])))
 }
 
+func puzzle2(adapters []int) {
+	adapters = append(adapters, 0)
+	sort.Ints(adapters)
+	graphs := make([]*graph.Graph, 0)
+	continuous := []int{0}
+	previous := 0
+	for i := 1; i < len(adapters); i++ {
+		if adapters[i] - previous != 3 {
+			continuous = append(continuous, adapters[i])
+		} else {
+			if len(continuous) > 1 {
+				graphs = append(graphs, createGraph(continuous))
+			}
+			// start continuous over
+			continuous = []int{adapters[i]}
+		}
 
+		previous = adapters[i]
+	}
+
+	// if we have continous left over, create a graph for it
+	if len(continuous) > 1 {
+		graphs = append(graphs, createGraph(continuous))
+	}
+
+	numPaths := 1
+	for _, g := range graphs {
+		if len(g.Paths) > 0 {
+			numPaths *= len(g.Paths)
+		}
+	}
+	println(fmt.Sprintf("Puzzle Two: %d", numPaths))
+}
+
+func createGraph(continuous []int) *graph.Graph {
+	g := graph.NewGraph(len(continuous))
+	for j := 0; j < len(continuous); j++ {
+		for k := j + 1; k <= j + 3 && k < len(continuous); k++ {
+			g.AddEdge(continuous[j], continuous[k])
+		}
+	}
+	g.CreatePaths(continuous[0], continuous[len(continuous) - 1])
+	return g
+}
---
+++ internal/graph/graph.go
@@ -0,0 +1,63 @@
+package graph
+
+type Graph struct {
+	Paths [][]int
+	vertices int
+	edges map[int][]int
+}
+
+func NewGraph(vertices int) *Graph {
+	//edges := make([][]int, 0)
+	//for i := 0; i < vertices; i++ {
+	//	edges[i] = make([]int, 0)
+	//}
+	return &Graph{
+		Paths: make([][]int, 0),
+		vertices: vertices,
+		edges: make(map[int][]int),
+	}
+}
+
+func (g *Graph) AddEdge(u, v int) {
+	if _, ok := g.edges[u]; !ok {
+		g.edges[u] = make([]int, 0)
+	}
+	g.edges[u] = append(g.edges[u], v)
+}
+
+func (g *Graph) CreatePaths(start, end int) {
+	visited := make(map[int]bool)
+	pathList := []int {start}
+
+	g.calculatePaths(start, end, visited, pathList)
+}
+
+func (g *Graph) calculatePaths(node, end int, visited map[int]bool, path []int) {
+	if node == end {
+		g.Paths = append(g.Paths, path)
+		return
+	}
+
+	visited[node] = true
+
+	for _, v := range g.edges[node] {
+		if !visited[v] {
+			path = append(path, v)
+			g.calculatePaths(v, end, visited, path)
+
+			path = remove(path, v)
+		}
+	}
+
+	visited[node] = false
+}
+
+func remove(path []int, node int) []int {
+	newPath := make([]int, 0)
+	for _, p := range path {
+		if p != node {
+			newPath = append(newPath, p)
+		}
+	}
+	return newPath
+}
